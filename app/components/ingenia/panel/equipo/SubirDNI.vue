b37f2e9bcbabe83f7b30df8774f3d73286bfcbfa
Guillermo Croppi <guillermocroppi@gmail.com>
2018-03-29 18:44:10 -0300

    What did you said, BEITCH? Mejorado formulario de archivo

--- app/components/ingenia/panel/equipo/SubirDNI.vue
+++ app/components/ingenia/panel/equipo/SubirDNI.vue
@@ -8,23 +8,25 @@
     </div>
     <b-message>
       A continuación, por favor, suba una foto o escaneo de la parte posterior y anterior de su DNI
+      <br>Maximo: 3MB. Se aceptan .jpg o .jepg
     </b-message>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" ref="formDNI" method="post" enctype="multipart/form-data">
       <b-field class="file is-medium">
-        <b-upload v-model="files" name="documento">
+        <b-upload v-model="files" name="documento" accept="image/jpeg" v-validate="'required|size:3072|mimes:image/jpeg'">
           <a class="button is-link is-medium">
             <b-icon icon="upload"></b-icon>
             <span>Click para cargar</span>
           </a>
         </b-upload>
-        <span class="file-name" v-if="files && files.length">
-          {{ files[0].name }}
+        <span class="file-name" style="max-width: none;">
+          {{ files && files.length ? files[0].name : 'Seleccione un archivo para subir...' }}
         </span>
       </b-field>
+      <p v-show="errors.has('documento')" class="has-text-danger">Requerido. Debe ser una imagen .JPG de hasta 3MB como maximo </p>
       <div class="field">
         <div class="control is-clearfix">
-          <button @click.prevent="isLoading = true" type="submit" class="button is-primary is-medium is-pulled-right" :class="{'is-loading': isLoading}">
-            <i class="fa fa-paper-plane fa-fw"></i> Enviar</button>
+          <a @click="submit" type="submit" class="button is-primary is-medium is-pulled-right" :class="{'is-loading': isLoading}">
+            <i class="fa fa-paper-plane fa-fw"></i> Enviar</a>
         </div>
       </div>
     </form>
@@ -37,9 +39,34 @@ export default {
   data() {
     return {
       files: [],
-      isLoading: false,
-
+      isLoading: false
     };
+  },
+  methods: {
+    submit: function() {
+      this.$validator
+        .validateAll()
+        .then(result => {
+          if (!result) {
+            this.$snackbar.open({
+              message: "Error en el formulario. Verifíquelo",
+              type: "is-danger",
+              actionText: "Cerrar"
+            });
+            return false;
+          }
+          this.isLoading = true;
+          // this.$refs.formDNI.submit();
+        })
+        .catch(error => {
+          this.$snackbar.open({
+              message: "Error inesperado",
+              type: "is-danger",
+              actionText: "Cerrar"
+            });
+            return false;
+        });
+    }
   }
 };
 </script>
