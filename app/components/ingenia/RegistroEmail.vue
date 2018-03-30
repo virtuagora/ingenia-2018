9b2fa9e6d7b1eae83105d76ab707c958843f7576
Guillermo Croppi <guillermocroppi@gmail.com>
2018-03-30 20:18:12 -0300

    Fix of recaptcha

--- app/components/ingenia/RegistroEmail.vue
+++ app/components/ingenia/RegistroEmail.vue
@@ -90,7 +90,7 @@ export default {
           this.$http
             .post(this.signUpUrl, {
               identifier: this.email,
-              recaptcha: null 
+              recaptcha: this.recaptcha 
             })
             .then(response => {
               this.isLoading = false;
