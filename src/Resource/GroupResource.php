e90fa7c083a1cbde938a44ff16c8d844443bc920
Guillermo Croppi <guillermocroppi@gmail.com>
2018-04-02 09:27:42 -0300

    Fix en la verificacion de la fecha limite al crear un grupo

--- src/Resource/GroupResource.php
+++ src/Resource/GroupResource.php
@@ -10,192 +10,192 @@ class GroupResource extends Resource
     public function retrieveSchema($options = [])
     {
         $schema = [
-            'type' => 'object',
-            'properties' => [
-                'name' => [
-                    'type' => 'string',
-                    'minLength' => 1,
-                    'maxLength' => 100,
-                ],
-                'description' => [
-                    'type' => 'string',
-                    'minLength' => 1,
-                    'maxLength' => 1000,
-                ],
-                'year' => [
-                    'type' => 'integer',
-                    'minimum' => 1900,
-                    'maximun' => 2018,
-                ],
-                'previous_editions' => [
-                    'type' => 'array',
-                    'items' => [
-                        'type' => 'integer',
-                        'minimum' => 2000,
-                        'maximun' => 2017,
-                    ],
-                ],
-                'parent_organization' => [
-                    'oneOf' => [
-                        [
-                            'type' => 'object',
-                            'properties' => [
-                                'name' => [
-                                    'type' => 'string',
-                                    'minLength' => 1,
-                                    'maxLength' => 50,
-                                ],
-                                'topics' => [
-                                    'type' => 'array',
-                                    'items' => [
-                                        'type' => 'string',
-                                        'minLength' => 1,
-                                        'maxLength' => 100,
-                                    ],
-                                ],
-                                'topic_other' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'minLength' => 1,
-                                            'maxLength' => 250,
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                                'locality_id' => [
-                                    'type' => 'integer',
-                                    'minimum' => 1,
-                                ],
-                                'locality_other' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'minLength' => 1,
-                                            'maxLength' => 250,
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                                'web' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'minLength' => 1,
-                                            'maxLength' => 100,
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                                'facebook' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'minLength' => 1,
-                                            'maxLength' => 100,
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                                'telephone' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'minLength' => 1,
-                                            'maxLength' => 20,
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                                'email' => [
-                                    'oneOf' => [
-                                        [
-                                            'type' => 'string',
-                                            'format' => 'email',
-                                        ], [
-                                            'type' => 'null',
-                                        ],
-                                    ],
-                                ],
-                            ],
-                            'required' => [
-                                'name', 'topics', 'topic_other', 'locality_id',
-                                'locality_other', 'web', 'facebook', 'telephone', 'email',
-                            ],
-                            'additionalProperties' => false,
-                        ], [
-                            'type' => 'null',
-                        ],
-                    ],
-                ],
-                'locality_id' => [
-                    'type' => 'integer',
-                    'minimum' => 1,
-                ],
-                'locality_other' => [
-                    'oneOf' => [
-                        [
-                            'type' => 'string',
-                            'minLength' => 1,
-                            'maxLength' => 250,
-                        ], [
-                            'type' => 'null',
-                        ],
-                    ],
-                ],
-                'web' => [
-                    'oneOf' => [
-                        [
-                            'type' => 'string',
-                            'minLength' => 1,
-                            'maxLength' => 100,
-                        ], [
-                            'type' => 'null',
-                        ],
-                    ],
-                ],
-                'facebook' => [
-                    'oneOf' => [
-                        [
-                            'type' => 'string',
-                            'minLength' => 1,
-                            'maxLength' => 100,
-                        ], [
-                            'type' => 'null',
-                        ],
-                    ],
-                ],
-                'telephone' => [
-                    'type' => 'string',
-                    'minLength' => 1,
-                    'maxLength' => 20,
-                ],
-                'email' => [
-                    'type' => 'string',
-                    'format' => 'email',
-                ],
-            ],
-            'required' => [
-                'name', 'description', 'year', 'previous_editions', 'parent_organization',
-                'locality_id', 'locality_other', 'web', 'facebook', 'telephone', 'email',
-            ],
-            'additionalProperties' => false,
+        'type' => 'object',
+        'properties' => [
+        'name' => [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ],
+        'description' => [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 1000,
+        ],
+        'year' => [
+        'type' => 'integer',
+        'minimum' => 1900,
+        'maximun' => 2018,
+        ],
+        'previous_editions' => [
+        'type' => 'array',
+        'items' => [
+        'type' => 'integer',
+        'minimum' => 2000,
+        'maximun' => 2017,
+        ],
+        ],
+        'parent_organization' => [
+        'oneOf' => [
+        [
+        'type' => 'object',
+        'properties' => [
+        'name' => [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 50,
+        ],
+        'topics' => [
+        'type' => 'array',
+        'items' => [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ],
+        ],
+        'topic_other' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 250,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'locality_id' => [
+        'type' => 'integer',
+        'minimum' => 1,
+        ],
+        'locality_other' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 250,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'web' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'facebook' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'telephone' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 20,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'email' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'format' => 'email',
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        ],
+        'required' => [
+        'name', 'topics', 'topic_other', 'locality_id',
+        'locality_other', 'web', 'facebook', 'telephone', 'email',
+        ],
+        'additionalProperties' => false,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'locality_id' => [
+        'type' => 'integer',
+        'minimum' => 1,
+        ],
+        'locality_other' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 250,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'web' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'facebook' => [
+        'oneOf' => [
+        [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 100,
+        ], [
+        'type' => 'null',
+        ],
+        ],
+        ],
+        'telephone' => [
+        'type' => 'string',
+        'minLength' => 1,
+        'maxLength' => 20,
+        ],
+        'email' => [
+        'type' => 'string',
+        'format' => 'email',
+        ],
+        ],
+        'required' => [
+        'name', 'description', 'year', 'previous_editions', 'parent_organization',
+        'locality_id', 'locality_other', 'web', 'facebook', 'telephone', 'email',
+        ],
+        'additionalProperties' => false,
         ];
         return $schema;
     }
-
+    
     public function acceptInvitation($subject, $invId)
     {
         $user = $this->helper->getUserFromSubject($subject, ['groups']);
         $invitation = $this->db->query('App:Invitation')->where([
-            'id' => $invId,
-            'state' => 'pending',
-            'user_id' => $user->id,
+        'id' => $invId,
+        'state' => 'pending',
+        'user_id' => $user->id,
         ])->firstOrFail();
         if (count($user->groups)) {
             throw new AppException('User already has a group');
@@ -207,47 +207,47 @@ class GroupResource extends Resource
         $invitation->save();
         $user->groups()->attach($invitation->group_id, ['relation' => 'miembro']);
     }
-
+    
     public function inviteUser($subject, $group, $data)
     {
         $v = $this->validation->fromSchema([
-            'type' => 'object',
-            'properties' => [
-                'email' => [
-                    'type' => 'string',
-                    'format' => 'email',
-                ],
-            ],
-            'required' => ['email'],
-            'additionalProperties' => false,
+        'type' => 'object',
+        'properties' => [
+        'email' => [
+        'type' => 'string',
+        'format' => 'email',
+        ],
+        ],
+        'required' => ['email'],
+        'additionalProperties' => false,
         ]);
         $v->assert($data);
         $email = $data['email'];
-
+        
         $countInvit = $group->invitations()->count();
         if ($countInvit > 10) {
             throw new AppException('Invitations limit reached');
         }
         $prevInvit = $this->db->query('App:Invitation')->where([
-            'email' => $email,
-            'group_id' => $group->id
+        'email' => $email,
+        'group_id' => $group->id
         ])->first();
         if (isset($prevInvit)) {
             throw new AppException('User already invited');
         }
-
+        
         $user = $this->db->query('App:User')->where('email', $email)->first();
         if (isset($user)) {
             $invitation = $this->db->new('App:Invitation', [
-                'user_id' => $user->id,
-                'group_id' => $group->id
+            'user_id' => $user->id,
+            'group_id' => $group->id
             ]);
         } else {
             $invitation = $this->db->new('App:Invitation', [
-                'group_id' => $group->id
+            'group_id' => $group->id
             ]);
             $pending = $this->identity->createPendingUser('local', [
-                'identifier' => $email,
+            'identifier' => $email,
             ]);
             // $pending->fields = [
             //     'group_id' => $group->id,
@@ -263,14 +263,14 @@ class GroupResource extends Resource
         $invitation->save();
         return $invitation;
     }
-
+    
     public function createOne($subject, $data)
     {
         $v = $this->validation->fromSchema($this->retrieveSchema());
         $v->assert($data);
-
+        
         $localidad = $this->db->query('App:Locality')->findOrFail($data['locality_id']);
-
+        
         $user = $this->helper->getUserFromSubject($subject, ['groups']);
         if (count($user->groups)) {
             throw new AppException('Ya pertenece a un equipo');
@@ -278,20 +278,20 @@ class GroupResource extends Resource
         if (count($user->pending_tasks)) {
             throw new AppException('Aún debe completar su perfil de usuario');
         }
-
+        
         $deadline = Carbon::parse($this->options->getAutoloaded()['deadline']);
         $today = Carbon::now();
-        if ($deadline->gt($today)) {
+        if ($today->gt($deadline)) {
             throw new AppException('Application period is over');
         }
-
+        
         $subGr = $this->db->new('App:Subject');
         $subGr->display_name = $data['name'];
         $subGr->img_type = 0;
         $subGr->img_hash = md5($data['email']);
         $subGr->type = 'Group';
         $subGr->save();
-
+        
         $group = $this->db->new('App:Group');
         $group->name = $data['name'];
         $group->description = $data['description'];
@@ -308,17 +308,17 @@ class GroupResource extends Resource
         }
         $group->subject()->associate($subGr);
         $group->save();
-
+        
         $group->users()->attach($user->id, ['relation' => 'responsable']);
-
+        
         $this->db->table('subject_role')->insert([
-            'role_id' => 'group',
-            'subject_id' => $subGr->id,
+        'role_id' => 'group',
+        'subject_id' => $subGr->id,
         ]);
-
+        
         return $group;
     }
-
+    
     public function updateLetter($subject, $group, $file)
     {
         if (is_null($group->project->organization)) {
@@ -329,12 +329,12 @@ class GroupResource extends Resource
         }
         $fileMime = $file->getClientMediaType();
         $allowedMimes = [
-            'application/pdf' => 'pdf',
-            'invalid/pdf' => 'pdf',
-            'application/msword' => 'doc',
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
-            'image/jpeg' => 'jpg',
-            'image/pjpeg' => 'jpg',
+        'application/pdf' => 'pdf',
+        'invalid/pdf' => 'pdf',
+        'application/msword' => 'doc',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
+        'image/jpeg' => 'jpg',
+        'image/pjpeg' => 'jpg',
         ];
         if (!isset($allowedMimes[$fileMime])) {
             throw new AppException('Tipo de documento inválido');
@@ -343,14 +343,14 @@ class GroupResource extends Resource
         $group->save();
         $fileStrm = $file->getStream()->detach();
         $this->filesystem->putStream(
-            'avales/'.$group->id.'.'.$allowedMimes[$fileMime],
-            $fileStrm
+        'avales/'.$group->id.'.'.$allowedMimes[$fileMime],
+        $fileStrm
         );
         if (is_resource($fileStrm)) {
             fclose($fileStrm);
         }
     }
-
+    
     public function updateAgreement($subject, $group, $file)
     {
         if ($file->getError() !== UPLOAD_ERR_OK) {
@@ -358,12 +358,12 @@ class GroupResource extends Resource
         }
         $fileMime = $file->getClientMediaType();
         $allowedMimes = [
-            'application/pdf' => 'pdf',
-            'invalid/pdf' => 'pdf',
-            'application/msword' => 'doc',
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
-            'image/jpeg' => 'jpg',
-            'image/pjpeg' => 'jpg',
+        'application/pdf' => 'pdf',
+        'invalid/pdf' => 'pdf',
+        'application/msword' => 'doc',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
+        'image/jpeg' => 'jpg',
+        'image/pjpeg' => 'jpg',
         ];
         if (!isset($allowedMimes[$fileMime])) {
             throw new AppException('Tipo de documento inválido');
@@ -372,8 +372,8 @@ class GroupResource extends Resource
         $group->save();
         $fileStrm = $file->getStream()->detach();
         $this->filesystem->putStream(
-            'acuerdos/'.$group->id.'.'.$allowedMimes[$fileMime],
-            $fileStrm
+        'acuerdos/'.$group->id.'.'.$allowedMimes[$fileMime],
+        $fileStrm
         );
         if (is_resource($fileStrm)) {
             fclose($fileStrm);
