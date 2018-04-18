e0fc9ef258d1612c990de7174e8f80f3bbafcc1d
Guillermo Croppi <guillermocroppi@gmail.com>
2018-04-18 14:10:51 -0300

    Pequeño update

--- app/components/ingenia/catalogo/Catalogo.vue
+++ app/components/ingenia/catalogo/Catalogo.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="columns">
-
+<section>
+  <br>
+  <div class="notification is-dark has-text-centered">
+    <i class="em em-muscle"></i> En construcción, ¡muy pronto estaremos en linea!
   </div>
+  <div class="columns">
+    <div class="column">
+      <div class="notification is-primary">
+        <a href="/proyecto/1" style="text-decoration:none;"><h1 class="title is-3">Escuela de Futbol Femenino Fighiera</h1></a>
+        <h1 class="subtitle is-4">Hay Futuro</h1>
+      </div>
+    </div>
+    <div class="column">
+<div class="notification is-primary">
+        <a href="/proyecto/2" style="text-decoration:none;"><h1 class="title is-3">Creando Vinculos</h1></a>
+        <h1 class="subtitle is-4">VinculARTE</h1>
+      </div>
+    </div>
+    <div class="column">
+<div class="notification is-primary">
+        <a href="/proyecto/3" style="text-decoration:none;"><h1 class="title is-3">Programa de concientización acerca de la alimentación saludable</h1></a>
+        <h1 class="subtitle is-4">Naturalmente</h1>
+      </div>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
